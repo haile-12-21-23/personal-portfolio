61bf9af8741b0999ff9da0749fb743a316e9fabe
haile-12-21-23 <hailemariamaddisu21@gmail.com>
2022-12-28 23:03:08 +0300

    experience page done and project page begin

--- src/components/pages/Project/Project.js
+++ src/components/pages/Project/Project.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import ProjectItem from './ProjectItem'
+// import Proj1 from "../../../assets/proj1.jpg"
+// import Proj3 from "../../../assets/proj3.webp"
+import "./Project.css"
+import { ProjectList } from '../../../helpers/ProjectList'
 function Project() {
     return (
-        <div>
-            Project
+        <div className="projects">
+            <h1>My Personal Projects</h1>
+            <div className="projectList">
+                {ProjectList.map((project) => {
+                    return <ProjectItem name={project.name} image={project.image} />
+                })}
+            </div>
         </div>
     )
 }
